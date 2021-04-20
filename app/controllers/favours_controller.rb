1197bd1532b2331a50b686dcf1b94fc5a63fd0f6
katsapvlad <katsapvlad@yandex.by>
2021-04-20 20:00:21 +0300

    Add favours output, flash alert, delete useless files

--- app/controllers/favours_controller.rb
+++ app/controllers/favours_controller.rb
@@ -1,23 +1,35 @@
 class FavoursController < ApplicationController
   def index
+    @favours = Favour.all
   end
 
   def new
   end
 
   def create
-    favour = Favour.new(favours_params)
-
+    favour = Favour.new(favour_params)
+    @favours = Favour.all
     if favour.save
       render action: 'index'
     else
+      #render :js => "alert(#{favour.errors.objects.first.full_message})"
+      #render js: "alert('test)";
+      #render plain: "#{favour.errors.objects.first.full_message}"
+       # render(
+       #   html: "<script>alert('#{favour.errors.objects.first.full_message}')</script>".html_safe,
+       #   layout: 'application'
+       # )
+      #flash.alert = "AAAAAAAAAAAAAAAAAAA"
+      #render action: 'new'
+      #flash.alert = "AAAAAAAAAAAAAAAAAAA"
+      flash.now[:notice] = "#{favour.errors.objects.first.full_message}"
       render action: 'new'
     end
   end
 
   private
 
-  def favours_params
+  def favour_params
     params.permit(:title, :description, :price, :image_link)
   end
 end
--- test/controllers/articles_creating_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ArticlesCreatingControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
