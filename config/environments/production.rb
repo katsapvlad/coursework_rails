699f716a34a6e1e63edf5adb01d48e872e09e7bc
katsapvlad <katsapvlad@yandex.by>
2021-05-05 05:07:07 +0300

    Add mailers settings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'ecodipmanager@gmail.com',
+  password:             'blyadskiyparol',
+  authentication:       'plain',
+  enable_starttls_auto: true }
+end
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
