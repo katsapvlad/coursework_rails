66d2ba490a4ad888ae329b3e63cf5f816b318518
katsapvlad <katsapvlad@yandex.by>
2021-05-05 03:59:47 +0300

    Delete useless migration

--- db/migrate/20210420145004_rewrite_favours.rb
+++
@@ -1,8 +0,0 @@
-class RewriteFavours < ActiveRecord::Migration[6.1]
-  def change
-    add_column :favours, :title, :string
-    add_column :favours, :description, :text
-    add_column :favours, :price, :integer
-    add_column :favours, :image_link, :string
-  end
-end
