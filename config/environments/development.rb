b3069af559a32b46cdbd5ab33fb301434113b5b3
Vlad Katsap <70981345+katsapvlad@users.noreply.github.com>
2021-06-09 11:45:42 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,7 +79,7 @@ config.action_mailer.smtp_settings = {
   port:                 587,
   domain:               'gmail.com',
   user_name:            'ecodipmanager@gmail.com',
-  password:             'blyadskiyparol',
+  password:             'zxXcb375!@8',
   authentication:       'plain',
   enable_starttls_auto: true }
 end
